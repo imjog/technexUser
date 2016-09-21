4f143d18ab325b41454d0bb22059bcd591e37cce
Bikram Bharti <bikram.bharti.ece15@itbhu.ac.in>
2016-09-22 01:33:22 +0530

    Events added

--- technexUser/settings.py
+++ technexUser/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'Auth',
 ]
 
@@ -52,6 +53,8 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'technexUser.urls'
