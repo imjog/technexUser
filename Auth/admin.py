19e72899eaeca57b2621684ae6cc9f4e701aed9f
Bikram Bharti <bikram.bharti.ece15@itbhu.ac.in>
2016-09-22 03:00:57 +0530

    Events added

--- Auth/admin.py
+++ Auth/admin.py
@@ -3,9 +3,16 @@ from django.contrib.auth.models import User
 # Register your models here.
 from Auth.models import *
 
+class eventOptionInline(admin.TabularInline):
+	model = EventOption
+	extra = 1
+
+@admin.register(Event)	
+class EventAdmin(admin.ModelAdmin):
+	inlines = [eventOptionInline]
 admin.site.register(TechProfile)
 admin.site.register(College)
-admin.site.register(Event)
+#admin.site.register(Event,EventAdmin)
 admin.site.register(ParentEvent)
 admin.site.register(Team)
 admin.site.register(EventOption)
