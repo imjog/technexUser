d4319dd8a5608e4dfdce3669c8887a80c670384e
Bikram Bharti <bikram.bharti.ece15@itbhu.ac.in>
2016-10-25 05:24:00 +0530

    whatev

--- Auth/views.py
+++ Auth/views.py
@@ -29,7 +29,7 @@ def contextCall(request):
         pass
     return response
 
-
+@csrf_exempt
 def register(request):
     if request.user.is_authenticated():
         return redirect('/dashboard')
@@ -59,7 +59,7 @@ def register(request):
         #print "codeBaes 2"
         subject = "Confirmation of Registration for Technex 2017"
         body = "Dear "+ data.get('name',None) +''',
-        
+
  You have successfully registered for Technex 2017. Team Technex welcomes you aboard!
 
 An important note to ensure that the team can contact you further:  If you find this email in Spam folder, please right click on the email and click on 'NOT SPAM'.
