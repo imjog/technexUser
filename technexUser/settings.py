b5225eec7de0115b05d039ddcedc5f972391ab46
rishabhiitbhu <rishabh.agrahari.eee15@iitbhu.ac.in>
2016-10-27 00:44:53 +0530

    debug

--- technexUser/settings.py
+++ technexUser/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a+lgm+6_g1ln9xl*=!u9galr&#&zaurt2%a*3ss)qt#xf0ck-9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # if 'DEBUG' in os.environ:
 #     if os.environ['DEBUG'] == 'FALSE':
 #         DEBUG = False
@@ -149,4 +149,4 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'
 
-CORS_ORIGIN_ALLOW_ALL = True 
+CORS_ORIGIN_ALLOW_ALL = True
