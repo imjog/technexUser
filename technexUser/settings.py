003e543cd2acc8321ad9867db6364a1f4f51d66d
Nitin <nitin.gera.ece14@iitbhu.ac.in>
2016-12-07 15:31:04 +0530

    static root bug fix

--- technexUser/settings.py
+++ technexUser/settings.py
@@ -133,7 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_HOST = 'https://d1guaaup0pib3t.cloudfront.net' if not DEBUG else ''
 STATIC_URL = STATIC_HOST + '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
 # STATICFILES_DIRS = (
