fb266091b202231591a58ec9d07bd1295b93eead
Nitin <nitin.gera.ece14@iitbhu.ac.in>
2016-12-07 14:58:53 +0530

    check cdn

--- technexUser/settings.py
+++ technexUser/settings.py
@@ -133,7 +133,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_HOST = 'https://d1guaaup0pib3t.cloudfront.net' if not DEBUG else ''
 STATIC_URL = STATIC_HOST + '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, 'static').replace('\\','/'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
