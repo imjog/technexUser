1a20d95f7e50e9fd38e790163051ba9042d4b61f
ishu2502 <ishandeepgandhi@gmail.com>
2017-02-08 22:14:15 +0530

    economist enigma

--- static/genmain.js
+++ static/genmain.js
@@ -32,7 +32,7 @@ as:[1,1,1,1,1,1]
 {
 parentEvent: 'riqueza',
 events:['analiticity', 'bulls-floor', 'krackat', 'manthan', 'economists-enigma'],
-max:[2,0,0,4,2],
+max:[2,0,0,4,4],
 as:[1,1,1,1,1]
 },
 {
